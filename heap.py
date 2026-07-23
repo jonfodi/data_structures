99449e59514ace4bfd5895fd67735ea730efd6a7
Jonathan Fodi <masterfodi@gmail.com>
2026-07-23 01:36:11 -0400

    more stuff

--- heap.py
+++ heap.py
@@ -13,15 +13,19 @@ views = {
     "about":    [10, 200, 500],                        # 0 in window
     "blog":     [705, 800, 850, 990],                  # 4 in window
 }
-
+total_count = [5, 60, 720, 800, 900, 945, 955]
 
 def record(page_id, timestamp):
     if page_id in views:
         views[page_id].append(timestamp)
     else:
         views[page_id] = [timestamp]
+    
+    total_count.append(timestamp)
 
-
+# this is for when your data is monotomic. consecutive values (i0 -> i1 -> i2) are false until they're all true. 
+# the flip point is the cutoff
+# since the indicies of the timestamp list implicitly carry the time relation, all timestamps will be within 5 minutes once the first index > 5 minutes is hit
 def count(page_id) -> int:
     timestamps = views.get(page_id, [])
     return len(timestamps) - bisect_left(timestamps, CUTOFF)
@@ -31,9 +35,16 @@ def top_k(k) -> list:
     heap = []
     for page_id in views:
         heappush(heap, (count(page_id), page_id))
+        print(heap)
         if len(heap) > k:
-            heappop(heap)          # drop the weakest one we're holding
+            heappop(heap)
+            print(heap)
     return sorted(heap, reverse=True)
 
+def total_number_events(CUTOFF):
+    return len(total_count) - bisect_left(total_count, CUTOFF)
 
-print(top_k(3))
+res = top_k(3)
+print("result = ")
+print(res)
+print(total_number_events(CUTOFF))
